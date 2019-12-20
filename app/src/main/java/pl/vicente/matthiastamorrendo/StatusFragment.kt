4ca90a30ae12feafad94883bde50ff66c28e72ef
Vicente Merlo <vammerlo@inf.ufrgs.br>
2019-12-20 15:26:26 -0300

    menu

--- app/src/main/java/pl/vicente/matthiastamorrendo/StatusFragment.kt
+++ app/src/main/java/pl/vicente/matthiastamorrendo/StatusFragment.kt
@@ -4,6 +4,8 @@ package pl.vicente.matthiastamorrendo
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +23,10 @@ class StatusFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.main_menu, menu)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
     }
 }
